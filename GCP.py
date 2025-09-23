2362874abea9bbc4b3ca9c173388a9b313c9e169
Soares314 <calvinpauh@gmail.com>
2025-09-22 22:34:41 -0300

    Função de avaliar os grafos

--- GCP.py
+++ GCP.py
@@ -16,13 +16,37 @@ def generate_random_colored_graphs(graph, number_colors, number_graphs):
     
     return random_graphs
 
-# Algoritmo genético para decidir o melhor grafo
-def color_graph(graph, number_colors, limit_of_generations):
-    random_10_graphs = generate_random_colored_graphs(graph, number_colors, 10)
+#Avalia os grafos pela quantidade de arestas cujo os nós tem cores diferentes 
+def evaluate_colored_graphs(colored_graphs):
+    colored_graphs_evaluated = {}
+    
+    for graph in colored_graphs:
+        graph_value = 0
+        
+        # Para cada dois nós ligados, verifica se tem cores diferente, se sim o grafo ganha mais valor
+        for node1, node2 in graph.edges():
+            if graph.nodes[node1]['color'] != graph.nodes[node2]['color']:
+                graph_value += 1
+                    
+        colored_graphs_evaluated.update({graph: graph_value})
+    
+    return colored_graphs_evaluated
+        
 
+# Algoritmo genético para decidir o grafo com melhor coloração
+def color_graph(graph, number_colors, limit_of_generations):
+    print(graph.number_of_edges())
+    
+    random_10_graphs = generate_random_colored_graphs(graph, number_colors, 10)
+    evaluated_graphs = evaluate_colored_graphs(random_10_graphs)
+    
+    print("Coloração ideal do grafo:")
+    for node in evaluated_graphs:
+        if evaluated_graphs[node] == graph.number_of_edges():
+            return node
 
 def main():
-    G = nx.Graph([(0, 1), (1, 2), (2, 0)])
+    G = nx.Graph([(0, 1), (1, 3),(1, 2), (2, 0), (0, 3)])
 
     color_graph(G, 3, 20)
 
